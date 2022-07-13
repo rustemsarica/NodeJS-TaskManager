645e380a5e8022709c0402825e689ab73ad99e14
rustemsarica <rustemsarica@gmail.com>
2022-07-13 12:52:30 +0300

    Added task sorting

--- public/browser-app.js
+++ public/browser-app.js
@@ -4,6 +4,8 @@ const formDOM = document.querySelector('.task-form')
 const taskInputDOM = document.querySelector('.task-input')
 const taskSearchInputDOM = document.querySelector('.task-search-input')
 const formAlertDOM = document.querySelector('.form-alert')
+const sortDOM = document.querySelector('.sort-select')
+
 // Load tasks from tasks
 const showTasks = async () => {
   loadingDOM.style.visibility = 'visible'
@@ -15,9 +17,11 @@ const showTasks = async () => {
       tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
       loadingDOM.style.visibility = 'hidden'
       taskSearchInputDOM.style.visibility = 'hidden'
+      sortDOM.style.visibility = 'hidden'
       return
     }else{
       taskSearchInputDOM.style.visibility = 'visible'
+      sortDOM.style.visibility = 'visible'
     }
     const allTasks = tasks
       .map((task) => {
@@ -85,6 +89,45 @@ const searchTasks = async (e) => {
 }
 taskSearchInputDOM.addEventListener('keyup', searchTasks)
 
+// sort tasks
+sortDOM.addEventListener('click', async (e) => {
+  const sortValue = e.target.value
+  const {
+    data: { tasks },
+  } = await axios.get('/tasks')
+
+  const sortedTasks = tasks.sort((a, b) => {
+    if (sortValue === 'name-asc') {
+      return a.name.localeCompare(b.name)
+    } else if(sortValue === 'name-desc'){
+      return b.name.localeCompare(a.name)
+    } else if (sortValue === 'status-asc') {
+      return a.status - b.status
+    } else if (sortValue === 'status-desc') {
+      return b.status - a.status
+    }
+  })
+
+  const allTasks = sortedTasks
+    .map((task) => {
+      const { status, _id: taskID, name } = task
+      return `<div class="single-task ${status && 'task-status'}">
+              <h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
+              <div class="task-links">
+              <!-- edit link -->
+              <a href="task.html?id=${taskID}"  class="edit-link">
+              <i class="fas fa-edit"></i>
+              </a>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn" data-id="${taskID}">
+              <i class="fas fa-trash"></i>
+              </button>
+              </div>
+              </div>`
+    })
+    .join('')
+  tasksDOM.innerHTML = allTasks
+})
 
 // delete task /task/:id
 
