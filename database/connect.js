0ed7ea19cece162baed5eeda81a179e7a534deee
rustemsarica <rustemsarica@gmail.com>
2022-07-16 14:29:39 +0300

    update

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,3 @@
-const connectionString = 'mongodb+srv://task-manager:task-manager.97@cluster0.n3ukqce.mongodb.net/TaskManager?retryWrites=true&w=majority';
-
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
